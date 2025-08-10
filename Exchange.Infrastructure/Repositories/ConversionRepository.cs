c2f5164f7a5adb48428185b9e75fdb39cf2c44c9
DiegoLins10 <diegofernandeslins@gmail.com>
2025-08-10 01:32:37 -0300

    Criação do repository de conversion

--- Exchange.Infrastructure/Repositories/ConversionRepository.cs
+++ Exchange.Infrastructure/Repositories/ConversionRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Exchange.Domain.Entities;
+using Exchange.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Exchange.Infrastructure.Repositories
 {
-    public class ConversionRepository
+    public class ConversionRepository : IConversionRepository
     {
         private static readonly List<ConversionRecord> _data = new();
 
