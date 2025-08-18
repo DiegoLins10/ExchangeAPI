4a1a3cbdc6639e2b5a820c7f148a22683f10aee5
DiegoLins10 <diegofernandeslins@gmail.com>
2025-08-17 22:53:51 -0300

    Implementação requisicao bacen

---
+++ Exchange.Infrastructure/Services/Bacen/Responses/CurrencyBacenResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Exchange.Infrastructure.Services.Bacen.Responses
+{
+    public class CurrencyBacenResponse
+    {
+        public List<CurrencyItemBacen> Value { get; set; } = new();
+    }
+
+    public class CurrencyItemBacen
+    {
+        public decimal ParidadeCompra { get; set; }
+        public decimal ParidadeVenda { get; set; }
+        public decimal CotacaoCompra { get; set; }
+        public decimal CotacaoVenda { get; set; }
+        public DateTime DataHoraCotacao { get; set; }
+        public string TipoBoletim { get; set; } = string.Empty;
+    }
+}
